219310721b6a1acb8e7000978cb2fd5c5992bed0
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-06-04 11:35:06 -0400

    updated department-mapper

--- wordpress/wp-content/themes/dpsm-intranet/inc/mappers/department.php
+++ wordpress/wp-content/themes/dpsm-intranet/inc/mappers/department.php
@@ -1,9 +1,52 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 function dpsm_get_department(
-int $post_id
-): array
-{
-return [
-'id' => $post_id,
-'title' => get_the_title($post_id),
-];
+    int $post_id
+): array {
+
+    return [
+
+        'id' =>
+        $post_id,
+
+        'title' =>
+        get_the_title(
+            $post_id
+        ),
+
+        'department_code' =>
+        get_field(
+            'department_code',
+            $post_id
+        ),
+
+        'short_name' =>
+        get_field(
+            'short_name',
+            $post_id
+        ),
+
+        'department_head' =>
+        get_field(
+            'department_head',
+            $post_id
+        ),
+
+        'contact_email' =>
+        get_field(
+            'contact_email',
+            $post_id
+        ),
+
+        'contact_phone' =>
+        get_field(
+            'contact_phone',
+            $post_id
+        ),
+
+    ];
 }
